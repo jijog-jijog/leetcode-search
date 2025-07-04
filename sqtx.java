555d743fb678e977c18ee7cf5d915c625d0f9f62
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 12:51:15 +0530

    Fix input value of x, change ans type to int, and correct loop condition in sqtx.java

--- sqtx.java
+++ sqtx.java
@@ -1,15 +1,15 @@
 public class sqtx {
     public static void main(String[] args) {
-       int x = 12;
+       int x = 8;
        int left = 0;
        int right = x;
-       long ans = 0;
+       int ans = 0;
         
-       while (left >= right) {
+       while (left <= right) {
         int mid = (left + right)/2;
         long midsq = (long)mid*mid;
         if (midsq < x) {
-            mid = ans;
+            ans = mid;
             left = mid + 1;
         }else if (midsq > x) {
              right = mid-1;
