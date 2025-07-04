31323733e98b66da9daa8dfa8d76b6af111ee091
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 09:42:44 +0530

    Fix loop condition in sqtx.java to use left pointer for binary search

--- sqtx.java
+++ sqtx.java
@@ -3,10 +3,9 @@ public class sqtx {
         int x = 8;
         int left = 0;
         int right = x;
-        int ans =0;
 
-        while(right <= x){
-            
+        while(left <= x){
+             int ans =0;
             int mid = left + (x - left)/2;
             long midsq = (long)mid*mid;
             if (midsq == x){
