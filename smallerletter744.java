2cc0f1f6e3c906a3bf06834e624e1aa1a96270b1
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 22:13:32 +0530

    Refactor smallerletter744 class to use char array for letters and simplify target comparison logic

--- smallerletter744.java
+++ smallerletter744.java
@@ -1,26 +1,26 @@
 public class smallerletter744 {
     public static void main(String[] args) {
-        String[] letters = {"c","f","j"};
+        char[] letters = {'c' , 'f' , 'j'};
         char target = 'f';
         int left = 0;
         int right = letters.length - 1;
-        char ans = letters[0].charAt(0);
+        char ans = letters[0];
 
         while (left <= right) {
             int mid = left + (right - left)/2;
 
-            if (letters[mid].charAt(0) == target) {
-                ans = letters[mid + 1].charAt(0);
+            if (letters[mid] == target) {
+                ans = letters[mid];
 
-            }else if (letters[mid].charAt(0) > target ) {
+            }else if (letters[mid] > target ) {
                 right = mid - 1;
 
-            }else if(letters[mid].charAt(0) < target){
-                ans = letters[mid].charAt(0);
+            }else if(letters[mid] < target){
+                ans = letters[mid];
                 left = mid + 1;
 
             }else{
-                ans = letters[0].charAt(0);
+                ans = letters[0];
                 break;
             }
         }
