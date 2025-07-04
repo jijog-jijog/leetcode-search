7a059b5bbbdd2db2770c9f3e8366d59f336eb8cf
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 16:36:56 +0530

    Implement logic to check for pairs that sum to the target in sortedtwosum class

--- sortedtwosum.java
+++ sortedtwosum.java
@@ -5,7 +5,9 @@ public class sortedtwosum {
         
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
-                
+                if (nums[i] + nums[j] == target) {
+                    
+                }
             }
         }
     }
