e7e3adc13720c71fbaba26643f3f4fc3a8ca807e
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 11:56:26 +0530

    Fix condition in while loop to correctly compare midsq with x in sqtx.java

--- sqtx.java
+++ sqtx.java
@@ -8,11 +8,11 @@ public class sqtx {
        while (left >= right) {
         int mid = x/2;
         long midsq = (long)mid*mid;
-        if (midsq > x) {
+        if (midsq < x) {
             ans = midsq;
             left = mid + 1;
-        }else{
-            right = mid-1;
+        }else if (midsq > x) {
+             right = mid-1;
         }
        }
     System.out.println(ans);
