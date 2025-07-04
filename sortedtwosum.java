16212f2bec3f32729fc6a69e2a3a3cb50ae1d6e2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 16:39:21 +0530

    Add return statement for index pair in sortedtwosum class

--- sortedtwosum.java
+++ sortedtwosum.java
@@ -6,7 +6,7 @@ public class sortedtwosum {
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 if (nums[i] + nums[j] == target) {
-                    
+                   
                 }
             }
         }
