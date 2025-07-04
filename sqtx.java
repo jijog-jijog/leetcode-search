860d33cae6ac56598ce2bef295b8b7f0a6306b7e
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 11:48:39 +0530

    Initialize variables and set up while loop structure in sqtx.java

--- sqtx.java
+++ sqtx.java
@@ -1,5 +1,11 @@
 public class sqtx {
     public static void main(String[] args) {
-       
+       int x = 12;
+       int left = 0;
+       int right = x;
+        
+       while (condition) {
+        
+       }
     }
 }
