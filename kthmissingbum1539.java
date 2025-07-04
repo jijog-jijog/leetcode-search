a26f8ef8629384d6e6a6102982c05764969c9c13
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 23:31:03 +0530

    Fix calculation of result in kthmissingbum1539 class for accurate output

--- kthmissingbum1539.java
+++ kthmissingbum1539.java
@@ -1,13 +1,13 @@
 public class kthmissingbum1539 {
     public static void main(String[] args) {
-        int[] arr ={2,3,4,7,11};
+        int[] arr ={1,2,3,4};
         int k = 5;
         int ans = 0;
         int left =  0;
         int right = arr.length-1;
         while (left <= right) {
             int mid = left + (right - left)/2;
-            int res = arr[mid] - (mid + 1);
+            int res = arr[mid] - mid;
             if (res < k ) {
                 ans = res;
                 left = mid + 1;
