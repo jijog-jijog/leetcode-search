96e9c619f6d3ef0ee09e287a084ad32c832eb20c
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 23:27:08 +0530

    Implement initial version of kthmissingbum1539 class with binary search logic

---
+++ kthmissingbum1539.java
@@ -0,0 +1,20 @@
+public class kthmissingbum1539 {
+    public static void main(String[] args) {
+        int[] arr ={2,3,4,7,11};
+        int k = 5;
+        int ans = 0;
+        int left =  0;
+        int right = arr.length-1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            int res = arr[mid] - mid;
+            if (res < k ) {
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        }
+    }
+
