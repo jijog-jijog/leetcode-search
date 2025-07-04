23c5c0c8f4792d2e68fa907399adbbf3e1b6c539
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 22:08:11 +0530

    Fix target assignment in binary search logic of smallerletter744 class

--- smallerletter744.java
+++ smallerletter744.java
@@ -9,7 +9,7 @@ public class smallerletter744 {
         while (left <= right) {
             int mid = left + (right - left)/2;
             if (letters[mid].charAt(0) == target) {
-                ans = letters[mid].charAt(0);
+                ans = letters[mid + 1].charAt(0);
             }else if (letters[mid].charAt(0) > target ) {
                 right = mid - 1;
             }else if(letters[mid].charAt(0) < target){
