b02f2070c362aab3fdf0e8c38d4735c7e9af97f9
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 16:35:11 +0530

    Add target variable initialization in sortedtwosum class

--- sortedtwosum.java
+++ sortedtwosum.java
@@ -1,6 +1,7 @@
 public class sortedtwosum {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
+        int target = 9;
         
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
