1b46dee273cf2bda17be4d7796456bf9f311db7d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 23:39:09 +0530

    Fix value of k and adjust result calculation in kthmissingbum1539 class for accurate output

--- kthmissingbum1539.java
+++ kthmissingbum1539.java
@@ -1,21 +1,20 @@
 public class kthmissingbum1539 {
     public static void main(String[] args) {
         int[] arr ={1,2,3,4};
-        int k = 5;
+        int k = 2;
         int ans = 0;
         int left =  0;
         int right = arr.length-1;
         while (left <= right) {
             int mid = left + (right - left)/2;
-            int res = arr[mid] - mid;
+            int res = arr[mid] - mid+1;
             if (res < k ) {
-                ans = res;
                 left = mid + 1;
             }else{
                 right = mid - 1;
             }
         }
-    System.out.println(ans + k);
+    System.out.println(left + k);
         }
     }
 
