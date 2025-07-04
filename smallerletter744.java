c163c8a261b055c4dba1c1d3a78ac2d5a148d18e
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 21:46:50 +0530

    Implement initial structure for smallerletter744 class with binary search logic

---
+++ smallerletter744.java
@@ -0,0 +1,18 @@
+public class smallerletter744 {
+    public static void main(String[] args) {
+        String[] letters = {"c","f","j"};
+        char target = 'a';
+        int left = 0;
+        int right = letters.length - 1;
+        char ans = letters[0].charAt(0);
+
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (letters[mid].charAt(0) == target) {
+                ans = letters[mid].charAt(0);
+            }else if (letters[mid].charAt(0) ) {
+                
+            }
+        }
+    }
+}
