f28221b203ec9bfe23db4a82bb08aaf131893452
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-04 22:09:52 +0530

    Fix target character in smallerletter744 class for accurate binary search

--- smallerletter744.java
+++ smallerletter744.java
@@ -1,20 +1,24 @@
 public class smallerletter744 {
     public static void main(String[] args) {
         String[] letters = {"c","f","j"};
-        char target = 'c';
+        char target = 'f';
         int left = 0;
         int right = letters.length - 1;
         char ans = letters[0].charAt(0);
 
         while (left <= right) {
             int mid = left + (right - left)/2;
+
             if (letters[mid].charAt(0) == target) {
                 ans = letters[mid + 1].charAt(0);
+
             }else if (letters[mid].charAt(0) > target ) {
                 right = mid - 1;
+
             }else if(letters[mid].charAt(0) < target){
                 ans = letters[mid].charAt(0);
                 left = mid + 1;
+
             }else{
                 ans = letters[0].charAt(0);
                 break;
