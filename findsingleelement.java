c7b8772795ec3460e1941992b8a70cecc515c572
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 22:52:57 +0530

    Fix formatting issue in array initialization

--- findsingleelement.java
+++ findsingleelement.java
@@ -1,6 +1,6 @@
 public class findsingleelement {
     public static void main(String[] args) {
-        int[] nums = { 1 , 1 ,  2 ,  3  , 3 ,4 ,4,8,8};
+        int[] nums = { 1 , 1 ,  2 ,  3   , 3 ,4 ,4,8,8};
 
         int left = 0;
         int right = nums.length-1;
