3595ee9a1b6e78e52439876122967588616c18d2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 16:32:29 +0530

    Fix array declaration in countnegnums class for correct grid initialization

--- countnegnums.java
+++ countnegnums.java
@@ -1,8 +1,10 @@
 public class countnegnums {
     public static void main(String[] args) {
-         int[] grid = {{4,3,2,-1},
+         int[][] grid = {{4,3,2,-1},
                       {3,2,1,-1},
                       {1,1,-1,-2},
-                      {-1,-1,-2,-3}}
+                      {-1,-1,-2,-3}};
+         int left = 0;
+         int right = grid.length;
     }
 }
