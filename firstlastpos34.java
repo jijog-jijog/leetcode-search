0a63540c0d74396f6f19c10e5d036b45e4bebff8
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 20:33:46 +0530

    Add initial implementation of first and last position search in sorted array

---
+++ firstlastpos34.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class firstlastpos34 {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+       
+    }
+}
