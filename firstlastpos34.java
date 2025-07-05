5b34f43f45c04790dd278edd0c885403ab6001bf
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 20:37:35 +0530

    Add calculation of mid index in binary search loop

--- firstlastpos34.java
+++ firstlastpos34.java
@@ -13,6 +13,7 @@ public class firstlastpos34 {
         int right = nums.length-1;
 
         while (left <= right) {
+            int mid = left + (right - left)/2;
             
         }
        
