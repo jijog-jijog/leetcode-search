0ee4242088e313e9076696e1f0285f0178b937f2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 16:31:34 +0530

    Add initial implementation of countnegnums class

---
+++ countnegnums.java
@@ -0,0 +1,8 @@
+public class countnegnums {
+    public static void main(String[] args) {
+         int[] grid = {{4,3,2,-1},
+                      {3,2,1,-1},
+                      {1,1,-1,-2},
+                      {-1,-1,-2,-3}}
+    }
+}
