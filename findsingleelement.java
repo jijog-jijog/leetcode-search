cacc3af0146533e66e6f8b91a71d2c59ff876e0d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 21:46:38 +0530

    Add calculation for mid index in binary search implementation

--- findsingleelement.java
+++ findsingleelement.java
@@ -6,6 +6,7 @@ public class findsingleelement {
         int right = nums.length-1;
 
         while (left <= right) {
+            int mid = left + (right - left)/2;
             if (nums[mid]) {
                 
             }
