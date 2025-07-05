babe46dfdd2b82bca3a7482fce63f8455120a59f
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 20:36:10 +0530

    Initialize variables for binary search in first and last position search

--- firstlastpos34.java
+++ firstlastpos34.java
@@ -5,6 +5,12 @@ public class firstlastpos34 {
     public static void main(String[] args) {
         int[] nums = {5,7,7,8,8,10};
         int target = 8;
+
+        int ansleft = -1;
+        int ansright = -1;
+
+        int left = 0;
+        int right = nums.length-1;
        
     }
 }
