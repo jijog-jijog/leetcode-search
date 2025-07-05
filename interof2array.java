a86f077f8d98ee6de4a9b9124859f628b49e1ac1
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 18:34:16 +0530

    Add intersection logic for two arrays

---
+++ interof2array.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class interof2array {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j])
+                ans.add(i);
+            }
+        }
+    
+
+
+    }
+}
