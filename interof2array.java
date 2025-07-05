de614b33bd2eea1d5dab3f1d365d69a4c0f304ed
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 18:40:18 +0530

    Fix result array initialization and correct intersection logic in array intersection

--- interof2array.java
+++ interof2array.java
@@ -7,11 +7,11 @@ public class interof2array {
         int[] nums2 = {2,2};
         
         List<Integer> ans = new ArrayList<>();
-        int[] res = new int[ans.size()];
+        int[] res = new int[nums1.length];
         for(int i=0; i<nums1.length; i++){
             for(int j=0; j<nums2.length; j++){
                 if(nums1[i] == nums2[j])
-                ans.add(i);
+                ans.add(nums1[i]);
             }
         }
     for(int i = 0; i < ans.size(); i++){
