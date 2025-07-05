5bc7c4268f71833ed88a41ba52477fc229dbf483
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 18:37:32 +0530

    Initialize result array for storing intersection indices

--- interof2array.java
+++ interof2array.java
@@ -7,14 +7,15 @@ public class interof2array {
         int[] nums2 = {2,2};
         
         List<Integer> ans = new ArrayList<>();
+        int[] res = new int[ans.size()];
         for(int i=0; i<nums1.length; i++){
             for(int j=0; j<nums2.length; j++){
                 if(nums1[i] == nums2[j])
                 ans.add(i);
             }
         }
-    for(List[] a : ans){
-        
+    for(int i = 0; i < ans.size(); i++){
+        res[i] = ans.get(i);
     }
 
 
