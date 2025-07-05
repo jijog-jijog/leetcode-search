627566226efccbcbbef6007e9e64cd93dcd9fd56
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 21:43:55 +0530

    Implement initial setup for finding single element in sorted array

---
+++ findsingleelement.java
@@ -0,0 +1,9 @@
+public class findsingleelement {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,8,8};
+
+        int left = 0;
+        int right = nums.length-1;
+        
+    }
+}
