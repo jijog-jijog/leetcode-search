29fec43af2faf1d3b4a2276505cfca072774d280
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 17:30:45 +0530

    Implement binary search logic for counting negative numbers in grid

--- countnegnums.java
+++ countnegnums.java
@@ -11,8 +11,14 @@ public class countnegnums {
          int left = 0;
          int right = len -1;
 
+         int ind = 0;
          for(int[] a : grid){
-            
+            while (left <= right ) {
+                int mid = left + (right - left)/2;
+                if (a[mid] < 0) {
+                    
+                }
+            }
          }
 
 }
