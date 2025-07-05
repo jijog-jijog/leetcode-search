e9eab1777ef0e23eb81d3645723a717602037be9
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 22:49:15 +0530

    Fix formatting issue in mid calculation of binary search implementation

--- findsingleelement.java
+++ findsingleelement.java
@@ -7,7 +7,7 @@ public class findsingleelement {
         int ans = 0;
 
         while (left <= right) {
-            int mid = left + (right - left)/2;
+            int mid = left + (right -  left)/2;
             if (nums[mid] !=  nums[mid + 1]) {
                 ans = nums[mid];
                 left = mid + 1;
