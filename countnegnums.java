a460f814315412679391733047e5f0f55dd11b38
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-05 17:28:02 +0530

    Refactor variable initialization for clarity in countnegnums class

--- countnegnums.java
+++ countnegnums.java
@@ -1,15 +1,16 @@
+import java.util.Arrays;
+
 public class countnegnums {
     public static void main(String[] args) {
          int[][] grid = {{4,3,2,-1},
                       {3,2,1,-1},
                       {1,1,-1,-2},
                       {-1,-1,-2,-3}};
+         int count = 0;
+         int len = grid.length;
          int left = 0;
-         int right = grid.length-1;
-         int ans = 0;
+         int right = len -1;
 
-         while (left <= right) {
-            
-         }
-    }
+         
+}
 }
