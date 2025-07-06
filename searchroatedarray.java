cbc63725fb65165f41fc1fff9b3915e5e93429e2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-06 19:25:12 +0530

    Implement binary search midpoint calculation in searchroatedarray main method

--- searchroatedarray.java
+++ searchroatedarray.java
@@ -6,7 +6,7 @@ public class searchroatedarray {
         int left = 0;
         int right = nums.length-1;
         while (left <= right) {
-            
+           int mid = left + (right - left)/2; 
         }
     }
 }
