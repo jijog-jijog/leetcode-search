ceba1f5308fba8f73dd3f03d3478269dcd65f792
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-07 09:16:11 +0530

    fix: update array initialization and adjust binary search logic

--- sortarray2.java
+++ sortarray2.java
@@ -1,6 +1,6 @@
 public class sortarray2 {
     public static void main(String[] args) {
-        int[] nums = {1,0,1,1,1};
+        int[] nums = {3,1};
         int target = 0;
         int left = 0;
         int right = nums.length-1;
@@ -12,7 +12,11 @@ public class sortarray2 {
             if (nums[mid] == target) {
                 ans = true;
             }
-            if (nums[mid] >= nums[left] ) {
+             if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            else if (nums[mid] >= nums[left] ) {
                 if (nums[mid] > target) {
                     right = mid - 1;
                 }else{
