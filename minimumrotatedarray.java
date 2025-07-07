cb30727b09732aef6be53c06175aa8af71fc6a26
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-07 10:01:15 +0530

    e

--- minimumrotatedarray.java
+++ minimumrotatedarray.java
@@ -1,6 +1,6 @@
 public class minimumrotatedarray {
     public static void main(String[] args) {
-        int[] nums = {11,13,15,17};
+        int[] nums = {3,4,5,1,2};
         int left = 0;
         int right = nums.length-1;
         int small = nums[0];
