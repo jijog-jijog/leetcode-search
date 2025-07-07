ef25d2c68724cb21c04df9223b7af3a993b8bb5c
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-07 10:45:26 +0530

    fix: correct peek calculation and update input array for accurate output

--- peek.java
+++ peek.java
@@ -1,19 +1,19 @@
 public class peek {
     public static void main(String[] args) {
-        int[] nums = {1,2,1,3,5,6,4};
+        int[] nums = {1,2,3,1};
         int left = 0;
         int right = nums.length-1;
-        int peek = 0;
+        int peek = nums[0];
 
         while (left <= right ) {
             int mid = left + (right - left)/2;
-            if (nums[left] > nums[mid] ) {
-                peek = mid;
-                right = mid - 1;
+            if (nums[mid] > peek ) {
+                peek = nums[mid];
+                 left = mid + 1;
             }else{
-                left = mid + 1;
+                 right = mid - 1;
             }
         }
-        System.out.println(peek);
+        System.out.println(nums[peek]);
     }
 }
