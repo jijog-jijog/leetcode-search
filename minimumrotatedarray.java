21f3ec2b3c2c86a7bb1bcfc59c553ebc4b9034b6
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-07 10:20:03 +0530

    fix: update input array in minimumrotatedarray and clean up commented code

--- minimumrotatedarray.java
+++ minimumrotatedarray.java
@@ -1,36 +1,37 @@
 public class minimumrotatedarray {
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {11,13,15,17};
         int left = 0;
         int right = nums.length-1;
         int small = nums[0];
 
         while (left <= right) {
             int mid = left + (right - left)/2;
-            if (nums[left] <= nums[mid]) {
+            // if (nums[left] <= nums[mid]) {
                 if (nums[mid] < small) {
                     small = nums[mid];
                     right = mid-1;
                 }else{
                     left = mid + 1;
                 }  
+        // }
         }
-        }
-        int left0 = 0;
-        int right0 = nums.length-1;
-        int mid2 = left0+(right0 - left0 )/2;
-        if (nums[mid2] >= nums[right]) {
-            int left2 = mid2;
-            int right2 = nums.length-1;
-            while (left2 <= right2) {
-                int mid = left2 + (right2 - left2);
-                if (nums[mid] < small) {
-                    small = nums[mid];
-                }else{
-                    right = mid - 1;
-                }
-            }
-        }
+        // int left0 = 0;
+        // int right0 = nums.length-1;
+        // int mid2 = left0+(right0 - left0 )/2;
+        // if (nums[mid2] >= nums[right]) {
+        //     int left2 = mid2;
+        //     int right2 = nums.length-1;
+        //     while (left2 <= right2) {
+        //         int mid = left2 + (right2 - left2);
+        //         if (nums[mid] < small) {
+        //             small = nums[mid];
+        //             left = mid + 1;
+        //         }else{
+        //             right = mid - 1;
+        //         }
+        //     }
+        // }
     System.out.println(small);
     }
 }
