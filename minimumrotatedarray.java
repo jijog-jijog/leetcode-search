c720c048af3d28f6d4f6b33d5272af89db4f8d47
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-07 09:48:41 +0530

    fix: initialize array and set left, right, and small variables in minimumrotatedarray

--- minimumrotatedarray.java
+++ minimumrotatedarray.java
@@ -1,5 +1,8 @@
 public class minimumrotatedarray {
     public static void main(String[] args) {
-        
+        int[] nums = {4,5,6,7,0,1,2};
+        int left = 0;
+        int right = nums.length-1;
+        int small = nums[0];
     }
 }
