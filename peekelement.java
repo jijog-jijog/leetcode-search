18cda2ef8e83f7d12fce4ecc4cacbb61312d0eca
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 17:30:44 +0530

    e

--- peekelement.java
+++ peekelement.java
@@ -1,16 +1,16 @@
 public  class peekelement {
 
     public static void main(String[] args) {
-        int nums[] = {1,2,4,6,7,5,4,2};
+        int nums[] = {2,4,6,7,5,4,2};
 
         int left = 0;
         int right = nums.length-1;
 
-        while (right < left) {
+        while (left < right) {
             int mid = left + (right - left)/2;
 
             if (nums[mid] < nums[mid + 1]) {
-                left = mid + 1;
+                 left = mid + 1;
             }else{
                 right = mid - 1;
             }
