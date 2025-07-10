7ddd94b9430c22847cd5215b7e82ef5e5bb24372
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 16:13:47 +0530

    Refactor binary search logic and update input array for clarity

--- infinitearray.java
+++ infinitearray.java
@@ -1,18 +1,33 @@
 public class infinitearray {
     public static void main(String[] args) {
-        int[] nums = {1,2,4,6,7,9,12,15,17,19,20,22,27,38,39,40,50,57,58,60,88,90};
-        int target = 27;
+        int[] nums = {1,2,4,6,7,9,12,15};
+        int target = 9;
 
         int left = 0;
         int right = 1;
+        int ans = -1;
 
        
-        while (nums[left] < target) {
-            int temp = right + 1;
+        while (target > nums[right]) {
+            int temp = right;
               right = right+(right - left + 1) *2;
-              left = temp;
+              left = temp + 1;
 
         }
-        System.out.println(right);
+        while (left <= right) {
+            int mid = left +(right - left)/2;
+            // if (nums[mid] == target) {
+            //     ans = mid;
+              
+            }if (nums[mid] > target) {
+                right = mid - 1;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            }else{
+                ans
+            }
+        }
+        System.out.println(ans);
+        System.out.println(nums[5]);
     }
 }
