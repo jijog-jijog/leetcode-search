e5195a73f20d1b9d8f3dc5a8fdaed1784e59c705
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 16:19:31 +0530

    t

--- infinitearray.java
+++ infinitearray.java
@@ -1,14 +1,14 @@
 public class infinitearray {
     public static void main(String[] args) {
-        int[] nums = {1,2,4,6,7,9,12,15};
-        int target = 9;
+        int[] nums = {1,2,4,6,7,9,12,15,17,19,20,22,27,38,39,40,50,57,58,60,88,90};
+        int target = 27;
 
         int left = 0;
         int right = 1;
         int ans =  -1;
 
        
-        while (target > nums[right]) {
+        while (nums[right] < target) {
             int temp = right +1;
               right = right+(right - left + 1) *2;
               left = temp;
@@ -26,6 +26,6 @@ public class infinitearray {
             }
         }
         System.out.println(ans);
-        System.out.println(nums[5]);
+        System.out.println(nums[12]);
     }
 }
