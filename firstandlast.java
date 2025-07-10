40d4558e594d26bdb121def61aad3ffc53a5cc11
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 09:56:09 +0530

    g

---
+++ firstandlast.java
@@ -0,0 +1,43 @@
+public class firstandlast {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+
+        int left = 0;
+        int right = nums.length - 1;
+        int anslef = -1;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+
+            if (nums[mid] == target) {
+                anslef = mid;
+                right = mid;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        int rleft = 0;
+        int rright = nums.length - 1;
+        int rans = -1;
+        while (rleft < rright){
+            int mid = rleft + (rright - rleft)/2;
+            if (nums[mid] == target) {
+                rans = mid;
+                rleft = mid;
+                
+            }else if (nums[mid] > target) {
+                rright = mid -1;
+            }else{
+                rleft = mid + 1;
+            }
+        } 
+
+        System.out.println(anslef + rans);
+            
+    }
+}
+    
