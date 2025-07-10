45f359619a40023dfe41b772c9effa9cfffbdc58
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 17:29:13 +0530

    e

--- peekelement.java
+++ peekelement.java
@@ -5,5 +5,16 @@ public  class peekelement {
 
         int left = 0;
         int right = nums.length-1;
+
+        while (right < left) {
+            int mid = left + (right - left)/2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        
     }
 }
