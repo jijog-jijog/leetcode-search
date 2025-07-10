e433d1b3085ef917eeddc52740023c5c30d858aa
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 20:10:53 +0530

    Add launch configuration for Solution1095 and update main method logic

--- Solution1095.java
+++ Solution1095.java
@@ -1,8 +1,60 @@
 public class Solution1095 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,3,1};
+        int[] arr = {1,2,4,5,3,1};
         
         int left = 0;
-        int right = 
+        int right = arr.length - 1;
+        int FH = left;
+        int target = 3;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid= left + (right - left)/2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                FH = left;
+                right = mid - 1;
+                
+            }else{
+                left = mid + 1;
+            }
+        }
+        System.out.println(FH);
+
+        int lleft = 0;
+        int lright = FH;
+
+        while (lleft <= lright) {
+            int mid = lleft +(lright - lleft)/2;
+            if (arr[mid] == target) {
+                ans = mid;
+                break;
+            }else if (arr[mid] > target) {
+                lright = mid - 1;
+            }else{
+                lleft = mid + 1;
+            }
+        }
+
+       
+
+        if (ans == -1) {
+            int l = FH + 1;
+            int r = arr.length - 1;
+
+            while (l < r) {
+                int mid = l +(r - l)/2;
+            if (arr[mid] == target) {
+                ans = mid;
+                break;
+            }else if (arr[mid] > target) {
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+            }
+        }
+         System.out.println(ans);
+
     }
 }
