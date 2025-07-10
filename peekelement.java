41cd43cdfb710be93286a2d28cd34cd673963087
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-10 17:30:10 +0530

    e

--- peekelement.java
+++ peekelement.java
@@ -1,7 +1,7 @@
 public  class peekelement {
 
     public static void main(String[] args) {
-        int nums[] = {1,2,4,6,7,5,4,2,1};
+        int nums[] = {1,2,4,6,7,5,4,2};
 
         int left = 0;
         int right = nums.length-1;
@@ -15,6 +15,6 @@ public  class peekelement {
                 right = mid - 1;
             }
         }
-        System.out.println(left);
+        System.out.println(nums[left]);
     }
 }
